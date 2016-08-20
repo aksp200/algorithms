787bc81595c7015be25355238084fb28d4691ec5
Akshay <aksmakesit@gmail.com>
2016-08-20 16:48:19 -0400

    Initial Commit

---
+++ Algorithms/src/org/aks/algo/Fibonacci.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.aks.algo;
+
+/**
+ * @author akshay
+ *
+ */
+public class Fibonacci {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
