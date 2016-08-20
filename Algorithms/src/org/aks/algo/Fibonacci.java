b8924befdf6d5717e35d1918b503294432e0b7d3
Akshay <aksmakesit@gmail.com>
2016-08-20 18:24:35 -0400

    Fibonacci series

--- Algorithms/src/org/aks/algo/Fibonacci.java
+++ Algorithms/src/org/aks/algo/Fibonacci.java
@@ -3,6 +3,10 @@
  */
 package org.aks.algo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author akshay
  *
@@ -13,8 +17,35 @@ public class Fibonacci {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Fibonacci fibonacci = new Fibonacci();
+		
+		int limit = 7;
+		System.out.println(fibonacci.fibonacci(limit));
+	}
+	
+	
+	public String readLine(Scanner scanner,String message){
+		String nextLine = null;
+		System.out.println(message);
+		nextLine = scanner.nextLine();
+		return nextLine;
+	}
+	
+	
+	public List<Integer> fibonacci(int limit){
+		List<Integer> fibonacci = new ArrayList<>();
+		int prev =0 ;
+		int current=0;
+		int next=1;
+		
+		while(limit-->0){
+			fibonacci.add(current);
+			prev=current;
+			current=next;
+			next = prev+current;
+		}
+		return fibonacci;
 	}
 
 }
