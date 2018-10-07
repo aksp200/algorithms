438880757eafca3e956c2764f8b9a5a7ce4bb6d6
Akshay Palshikar <8948642+akshaypalshikar@users.noreply.github.com>
2018-10-06 19:19:21 -0700

    Str permutation (#4)
    * permutation of string
    
    * correcting spelling

---
+++ Algorithms/src/org/aks/algo/StringPermitations.java
@@ -0,0 +1,31 @@
+package org.aks.algo;
+
+import org.aks.util.IoUtil;
+
+import java.util.Scanner;
+
+public class StringPermitations {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String str = IoUtil.readLine(scanner, "Enter a string : ");
+        System.out.printf("permutations of string %s : \n", str);
+        permutation(str, "");
+    }
+
+    /**
+     * O(n^2 * n!)
+     *
+     * @param str    string for permutation
+     * @param prefix to be used for permutation with given string
+     */
+    private static void permutation(String str, String prefix) {
+        if (str.length() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                permutation(rem, prefix + str.charAt(i));
+            }
+        }
+    }
+}
