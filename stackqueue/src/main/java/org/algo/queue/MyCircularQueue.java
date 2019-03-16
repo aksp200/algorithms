501ee4d146406d9ed7fe8ca3ab9ee9c512e4d0dc
Akshay Palshikar <aksmakesit@gmail.com>
2019-03-17 00:37:58 +0530

    implementation for circular queue using array

--- stackqueue/src/main/java/org/algo/queue/MyCircularQueue.java
+++ stackqueue/src/main/java/org/algo/queue/MyCircularQueue.java
@@ -1,14 +1,11 @@
 package org.algo.queue;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * https://leetcode.com/explore/learn/card/queue-stack/228/first-in-first-out-data-structure/1337/
  */
 public class MyCircularQueue {
 
-    private List<Integer> queue;
+    private Integer queue[];
     private int size;
     int head;
     int tail;
@@ -18,7 +15,7 @@ public class MyCircularQueue {
      * Initialize your data structure here. Set the size of the queue to be k.
      */
     public MyCircularQueue(int k) {
-        queue = new ArrayList<Integer>(k);
+        queue = new Integer[k];
         size = k;
         head = -1;
         tail = -1;
@@ -44,7 +41,7 @@ public class MyCircularQueue {
             head = getNext(head, size);
         }
         tail = getNext(tail, size);
-        queue.add(tail, value);
+        queue[tail] = value;
         currentSize++;
         return true;
     }
@@ -56,9 +53,14 @@ public class MyCircularQueue {
         if (currentSize <= 0) {
             return false;
         }
-        queue.remove(head);
-        head = getNext(head, size);
+        queue[head] = null;
         currentSize--;
+        if(head==tail){
+            head=-1;
+            tail=-1;
+            return true;
+        }
+        head = getNext(head, size);
         return true;
     }
 
@@ -66,14 +68,20 @@ public class MyCircularQueue {
      * Get the front item from the queue.
      */
     public int Front() {
-        return queue.get(head);
+        if (head == -1) {
+            return head;
+        }
+        return queue[head];
     }
 
     /**
      * Get the last item from the queue.
      */
     public int Rear() {
-        return queue.get(tail);
+        if (tail == -1) {
+            return tail;
+        }
+        return queue[tail];
     }
 
     /**
@@ -101,17 +109,40 @@ public class MyCircularQueue {
         }
         return current;
     }
-
-/*    public static void main(String args[]) {
+/*
+    public static void main(String args[]) {
 
         // Your MyCircularQueue object will be instantiated and called as such:
+
+         //["MyCircularQueue","enQueue","Rear","Rear","deQueue","enQueue","Rear","deQueue","Front","deQueue","deQueue","deQueue"]
+         //[[6],[6],[],[],[],[5],[],[],[],[],[],[]]
+
+
+
         MyCircularQueue obj = new MyCircularQueue(5);
+
         boolean param_1 = obj.enQueue(1);
-        boolean param_2 = obj.deQueue();
-        int param_3 = obj.Front();
-        int param_4 = obj.Rear();
-        boolean param_5 = obj.isEmpty();
-        boolean param_6 = obj.isFull();
+        int param_2 = obj.Rear();
+        int param_3 = obj.Rear();
+
+        boolean param_4 = obj.deQueue();
+        boolean param_5 = obj.enQueue(2);
+
+        int param_6 = obj.Rear();
+        boolean param_7 = obj.deQueue();
+
+        int param_8 = obj.Front();
+        boolean param_9 = obj.deQueue();
+        boolean param_10 = obj.deQueue();
+        boolean param_11 = obj.deQueue();
+
+        boolean param_12 = obj.isEmpty();
+        boolean param_13 = obj.isFull();
+
+
+        //["MyCircularQueue","enQueue","Rear","Rear","deQueue","enQueue","Rear","deQueue","Front","deQueue","deQueue","deQueue"]
+
+
     }*/
 }
 
