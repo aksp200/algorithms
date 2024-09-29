e754059f61416eaffb717bdf5e440026e93ee8c2
a0p0q6l <akshayganesh.palshik@walmart.com>
2024-09-29 18:35:34 +0530

    triplet sum

---
+++ src/org/aks/algo/TripletSum.java
@@ -0,0 +1,14 @@
+package org.aks.algo;
+
+public class TripletSum {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,15};
+        int s = 18;
+    }
+
+    static arr[][] triplets(int arr[],int s){
+        for(int i=0;i<arr.length-2;i++){
+            
+        }
+    }
+}
