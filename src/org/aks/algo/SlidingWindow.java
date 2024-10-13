034670bf6b19bd216a704def02732479f750fb1c
a0p0q6l <akshayganesh.palshik@walmart.com>
2024-10-13 13:05:58 +0530

    triplet sum and sliding window problem

---
+++ src/org/aks/algo/SlidingWindow.java
@@ -0,0 +1,11 @@
+package org.aks.algo;
+/**
+ * Housing
+Along one side of a road there is a sequence of vacant plots of land Each plot has a different area (non-zero). So, the areas form a sequence A[1], A[2], ... A[NI.
+You want to buy K acres of land to build a house. You want to find all segments of continguous plots (i.e., a subsection in the sequence) of whose sum is exactly K.
+ */
+public class SlidingWindow {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/org/aks/algo/TripletSum.java
+++ src/org/aks/algo/TripletSum.java
@@ -1,5 +1,15 @@
 package org.aks.algo;
+/**
+ * Triplets
 
+Given an array containing N integers, and an number S denoting a target sum.
+
+Find all distinct integers that can add up to form target sum. 
+The numbers in each triplet should be ordered in ascending order, and triplets should be ordered too.
+
+Return empty array if no such triplet exists.
+ * 
+ */
 public class TripletSum {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9,15};
